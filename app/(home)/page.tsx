c08d870f8ecdc7f71811c5aa6354d4c48d805c21
SungminPark1997 <72503832+SungminPark1997@users.noreply.github.com>
2024-02-11 17:40:18 +0900

    server side rendering

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,14 @@
 export const metadata = {
   title: "Home",
 };
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+async function getMovie() {
+  const response = await fetch(URL);
+  const result = await response.json();
+  return result;
+}
 
-export default function home() {
-  return <h1>안녕하세용</h1>;
+export default async function HomePage() {
+  const movies = await getMovie();
+  return <div>{JSON.stringify(movies)}</div>;
 }
