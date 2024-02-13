a6e91783c4b5e5c87e3d3e0ee932886fd26215c2
SungminPark1997 <72503832+SungminPark1997@users.noreply.github.com>
2024-02-14 01:31:17 +0900

    use Suspense

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,24 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 async function getMovie() {
-  const response = await fetch(URL);
+  const response = await fetch(API_URL);
   const result = await response.json();
   return result;
 }
 
 export default async function HomePage() {
   const movies = await getMovie();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movie/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
